c9839c1db14216115d42f7400bd780470dd35f59
Pedro Castilho <cstl.px@gmail.com>
2021-11-13 09:22:44 -0300

    Interactive Newton-Raphson

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,19 @@
 module Main where
 
 import Lib (hello, fizzBuzz, fizzBuzzInteractive)
-import NewtonRaphson (findSqrt)
+import NewtonRaphson (findRoot)
+
+import Text.Read (readMaybe)
 
 main :: IO ()
-main = fizzBuzzInteractive
+main = do
+  putStrLn "Insira um número positivo:"
+  numInput <- getLine
+  putStrLn "Insira qual raiz você quer (inteiro, n > 0):"
+  nthRootInput <- getLine
+  case (readMaybe numInput :: Maybe Double, readMaybe nthRootInput :: Maybe Int) of
+    (Just n, Just k)
+      | n > 0 && k > 0 -> putStrLn ("Resultado: " ++ (show (findRoot k n)))
+      | otherwise      -> putStrLn "Entrada inválida (números não-positivos)"
+
+    _ -> putStrLn "Entrada inválida."
--- src/NewtonRaphson.hs
+++ src/NewtonRaphson.hs
@@ -1,20 +1,10 @@
 module NewtonRaphson where
 
 findSqrt :: Double -> Double
-findSqrt n = findSqrt' n 100 (n / 2)
-
-findSqrt' _ 0     guess = guess
-findSqrt' n iters guess = findSqrt' n (iters - 1) (updateGuessSqrt n guess)
-
-updateGuessSqrt n guess = guess - (guess * guess - n) / (2 * guess)
+findSqrt = findRoot 2
 
 findCbrt :: Double -> Double
-findCbrt n = findCbrt' n 100 (n / 3)
-
-findCbrt' _ 0     guess = guess
-findCbrt' n iters guess = findCbrt' n (iters - 1) (updateGuessCbrt n guess)
-
-updateGuessCbrt n guess = guess - (guess * guess * guess - n) / (3 * guess * guess)
+findCbrt = findRoot 3
 
 findRoot :: Int -> Double -> Double
 findRoot n x
