d476156d869f6a92dbb70888899b3ddfc8594907
Pedro Castilho <cstl.px@gmail.com>
2021-11-13 09:00:11 -0300

    Cube root + Generic root

--- src/NewtonRaphson.hs
+++ src/NewtonRaphson.hs
@@ -4,6 +4,35 @@ findSqrt :: Double -> Double
 findSqrt n = findSqrt' n 100 (n / 2)
 
 findSqrt' _ 0     guess = guess
-findSqrt' n iters guess = findSqrt' n (iters - 1) (updateGuess n guess)
-  
-updateGuess n guess = guess - (guess * guess - n) / (2 * guess)
+findSqrt' n iters guess = findSqrt' n (iters - 1) (updateGuessSqrt n guess)
+
+updateGuessSqrt n guess = guess - (guess * guess - n) / (2 * guess)
+
+findCbrt :: Double -> Double
+findCbrt n = findCbrt' n 100 (n / 3)
+
+findCbrt' _ 0     guess = guess
+findCbrt' n iters guess = findCbrt' n (iters - 1) (updateGuessCbrt n guess)
+
+updateGuessCbrt n guess = guess - (guess * guess * guess - n) / (3 * guess * guess)
+
+findRoot :: Int -> Double -> Double
+findRoot n x
+  | n <  0    = error "Sem números complexos por enquanto, dsclp"
+  | n == 0    = error "Raiz zerésima é indefinida!"
+  | otherwise = findRootIter n x 100 (x / (fromIntegral n))
+
+findRootIter :: Int -> Double -> Int -> Double -> Double
+findRootIter _   _      0     guess = guess
+findRootIter nth target iters guess =
+  findRootIter nth target (iters - 1) (updateGuess nth target guess)
+
+updateGuess :: Int -> Double -> Double -> Double
+updateGuess nth target guess =
+  guess - (((pow guess nth) - target) / ((fromIntegral nth) * (pow guess (nth - 1))))
+
+pow :: Double -> Int -> Double
+pow _ 0 = 1
+pow m n
+  | n < 0     = 1 / (pow m (negate n))
+  | otherwise = m * (pow m (n - 1))
